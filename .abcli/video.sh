4c2163669421d5c9ed388cb52040ddc0c5acef6f
kamangir <arash@kamangir.net>
2024-01-02 20:48:55 -0800

    vanwatch use - kamangir/bolt#123

--- .abcli/video.sh
+++ .abcli/video.sh
@@ -5,7 +5,7 @@ export ABCLI_VIDEO_DEFAULT_SIZE="1280x1024"
 function abcli_create_video() {
     local task=$(abcli_unpack_keyword $1)
 
-    if [ "$task" == "help" ] ; then
+    if [ "$task" == "help" ]; then
         # https://esahubble.org/press/video_formats/
         abcli_show_usage "abcli create_video [<.jpg>] [<filename>] [fps=<10>,~rm_frames,resize_to=<$ABCLI_VIDEO_DEFAULT_SIZE>,scale=<1>]" \
             "$abcli_object_name/<.jpg> [-> <filename>.mp4]."
@@ -16,7 +16,7 @@ function abcli_create_video() {
 
     local video_filename=${suffix%%.*}
     local video_filename=$abcli_object_path/$(abcli_clarify_input $2 $video_filename).mp4
-    if [ -f "$video_filename" ] ; then
+    if [ -f "$video_filename" ]; then
         rm $video_filename
     fi
 
@@ -31,7 +31,7 @@ function abcli_create_video() {
     local extension=$(basename -- "$suffix")
     local extension="${extension##*.}"
 
-    if [ -d "temp_video" ] ; then
+    if [ -d "temp_video" ]; then
         rm -rf temp_video
     fi
     mkdir temp_video
@@ -39,23 +39,23 @@ function abcli_create_video() {
     local filename
     local index=0
     local size=""
-    for filename in *$suffix ; do
+    for filename in *$suffix; do
         local temp_filename=$abcli_object_path/temp_video/$index.$extension
 
         cp -v "$filename" $temp_filename
 
-        if [ ! -z "$resize_to" ] ; then
+        if [ ! -z "$resize_to" ]; then
             python3 -m abcli.plugins.video \
                 resize \
                 --filename $temp_filename \
-                --size $resize_to  
+                --size $resize_to
         fi
 
-        if [ -z "$size" ] ; then
+        if [ -z "$size" ]; then
             local size=$(python3 -m abcli.plugins.video size_of --filename $temp_filename --scale $scale)
         fi
 
-        (( index++ ))
+        ((index++))
     done
 
     abcli_log "$abcli_object_name/$suffix -> $video_filename - $size - $fps fps - 1/$scale"
@@ -68,7 +68,7 @@ function abcli_create_video() {
         -vcodec h264 \
         $video_filename
 
-    if [ "$rm_frames" == 1 ] ; then
+    if [ "$rm_frames" == 1 ]; then
         cp -v $abcli_object_path/temp_video/0.$extension $abcli_object_path/screenshot.$extension
         rm $abcli_object_path/*$suffix
     fi
--- Kanata/__init__.py
+++ Kanata/__init__.py
@@ -1,5 +1,5 @@
 NAME = "Kanata"
 
-VERSION = "2.199.1"
+VERSION = "2.200.1"
 
 from .utils import *
