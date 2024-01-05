f1df2abd5f619500146d21061ff45cf1e393cb9f
kamangir <arash@kamangir.net>
2024-01-04 19:44:43 -0800

    @create_video refactors - kamangir/bolt#123

--- .abcli/video.sh
+++ .abcli/video.sh
@@ -7,12 +7,13 @@ function abcli_create_video() {
 
     if [ $(abcli_option_int "$options" help 0) == 1 ]; then
         # https://esahubble.org/press/video_formats/
-        local options="jpg|png,fps=<10>,filename=<video>,prefix=<prefix>,rm_frames,resize_to=<$ABCLI_VIDEO_DEFAULT_SIZE>,scale=<1>"
+        local options="dryrun,jpg|png,fps=<10>,filename=<video>,gif,prefix=<prefix>,rm_frames,resize_to=<$ABCLI_VIDEO_DEFAULT_SIZE>,scale=<1>"
         abcli_show_usage "abcli create_video$ABCUL[$options]$ABCUL[.|<object-name>]" \
             "<object-name>/frames -> <filename>.mp4."
         return
     fi
 
+    local do_dryrun=$(abcli_option_int "$options" dryrun 0)
     local fps=$(abcli_option "$options" fps 10)
     local filename=$(abcli_option "$options" filename video)
     local prefix=$(abcli_option "$options" prefix)
@@ -20,6 +21,7 @@ function abcli_create_video() {
     local resize_to=$(abcli_option "$options" resize_to)
     local scale=$(abcli_option "$options" scale 1)
     local extension=$(abcli_option_choice "$options" jpg,png jpg)
+    local generate_gif=$(abcli_option_int "$options" gif 0)
 
     local object_name=$(abcli_clarify_object $2 .)
     local object_path=$abcli_object_root/$object_name
@@ -33,14 +35,16 @@ function abcli_create_video() {
     local temp_path=$object_path/$(abcli_string_random)
     mkdir -p $temp_path
 
-    local filename
+    local frame_filename
     local index=0
     local size=""
-    for filename in $object_path/$prefix*.$extension; do
-        abcli_log "🖼️  $filename"
+    for frame_filename in $object_path/$prefix*.$extension; do
+        abcli_log "🖼️  $frame_filename"
         local temp_filename=$temp_path/$index.$extension
 
-        cp -v "$filename" $temp_filename
+        cp -v \
+            $frame_filename \
+            $temp_filename
 
         [[ ! -z "$resize_to" ]] &&
             python3 -m abcli.plugins.video \
@@ -49,20 +53,28 @@ function abcli_create_video() {
                 --size $resize_to
 
         [[ -z "$size" ]] &&
-            local size=$(python3 -m abcli.plugins.video size_of --filename $temp_filename --scale $scale)
+            local size=$(python3 -m abcli.plugins.video size_of \
+                --filename $temp_filename \
+                --scale $scale)
 
         ((index++))
     done
 
     abcli_log "$object_name/$extension -> $video_filename - $size - $fps fps - 1/$scale"
 
-    abcli_eval - ffmpeg \
+    abcli_eval dryrun=$do_dryrun ffmpeg \
         -f image2 \
         -r $fps \
         -i $temp_path/%d.$extension \
         -s $size \
         -vcodec h264 \
         $video_filename
+
+    [[ "$generate_gif" == 1 ]] &&
+        abcli_eval dryrun=$do_dryrun \
+            ffmpeg -i \
+            $video_filename \
+            $object_path/$filename.gif
 
     if [ "$rm_frames" == 1 ]; then
         cp -v \
@@ -82,7 +94,7 @@ function abcli_create_video() {
         --content $size \
         --object_path $object_path
 
-    # rm -rfv $temp_path
+    rm -rfv $temp_path
 
     abcli_tag set $object_name video
 }
--- Kanata/__init__.py
+++ Kanata/__init__.py
@@ -1,5 +1,5 @@
 NAME = "Kanata"
 
-VERSION = "2.211.1"
+VERSION = "2.215.1"
 
 from .utils import *
